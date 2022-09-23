3dcbd37aae01ebe56623ff2c0ebe1c0bc2e9f757
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-09-22 22:18:58 -0300

    Implementação handle e command

---
+++ Campeonato.Domain.Shared/Command/Interfaces/ICommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Campeonato.Domain.Shared.Command.Interfaces;
+
+public interface ICommand
+{
+    #region Assignatures
+
+    bool Validate(out List<string> validationsErrors);
+
+    #endregion
+}
---
+++ Campeonato.Domain.Shared/Handler/Interfaces/IHandler.cs
@@ -0,0 +1,13 @@
+﻿using Campeonato.Domain.Shared.Command;
+using Campeonato.Domain.Shared.Command.Interfaces;
+
+namespace Campeonato.Domain.Shared.Handler.Interfaces;
+
+public interface IHandler<T> where T : ICommand
+{
+    #region Assignatures
+
+    CommandResult Handle(T command);
+
+    #endregion
+}
