2702e08518b606fa7408d0f84ab5423d9d0074db
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-09-23 08:01:18 -0300

    Implementação IRepositoryTeam

---
+++ Campeonato.Domain.Dados/Repositories/Interfaces/IRepositoryTeam.cs
@@ -0,0 +1,13 @@
+﻿using Campeonato.Domain.Entities;
+
+namespace Campeonato.Domain.Dados.Repositories.Interfaces;
+
+public interface IRepositoryTeam
+{
+    #region Assignatures
+
+    Task<IEnumerable<Team>> GetAllTeams();
+    Task<Team> Save(Team team);
+
+    #endregion
+}
