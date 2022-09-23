d3e3a24afbfb3842d837925302deae6a8b88f47f
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-09-23 08:18:20 -0300

    Implementação RepositoryTeam

---
+++ Campeonato.Domain.Dados/Repositories/RepositoryTeam.cs
@@ -0,0 +1,58 @@
+﻿using Campeonato.Domain.Dados.Data;
+using Campeonato.Domain.Dados.Repositories.Interfaces;
+using Campeonato.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Campeonato.Domain.Dados.Repositories;
+
+public class RepositoryTeam : IRepositoryTeam
+{
+    #region privates
+
+    private readonly DataContext _dataContext;
+
+    #endregion
+
+    #region Constructos
+
+    public RepositoryTeam(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public async Task<IEnumerable<Team>> GetAllTeams()
+    {
+        try
+        {
+            return await _dataContext
+                .Times
+                .AsNoTracking()
+                .ToListAsync();
+        }
+        catch (ArgumentNullException)
+        {
+            throw new Exception("Algum registro está nulo.");
+        }
+    }
+
+    public async Task<Team> Save(Team team)
+    {
+        try
+        {
+            await _dataContext.AddAsync(team);
+            await _dataContext.SaveChangesAsync();
+
+            return team;
+        }
+        catch (DbUpdateException)
+        {
+            throw new Exception("Erro ao salvar no banco.");
+        }
+    }
+
+    #endregion
+}
