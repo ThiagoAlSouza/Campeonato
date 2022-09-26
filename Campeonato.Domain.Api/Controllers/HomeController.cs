f6d57554f249417c21db1e14c82d83a37606b597
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-09-25 23:56:21 -0300

    Implementação HomeController

---
+++ Campeonato.Domain.Api/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Campeonato.Domain.Api.Controllers;
+
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    [Route("v1")]
+    public IActionResult Home()
+    {
+        return Ok();
+    }
+}
